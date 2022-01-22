15217f06c124637192a53d7fe13812900112e510
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-01-23 02:15:23 +0700

    fix profile icon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,89 +2,94 @@ import logo from '../assets/images/facebook-icon.svg';
 
 function Home() {
   return (
-    <header>
-      <nav className="navbar navbar-expand-sm navbar-light bg-white shadow-sm py-0">
-        <div className="container-fluid">
-          <div className="ms-1 flex-grow-1 py-2">
-            <a className="navbar-brand" href="/#">
-              <img src={logo} alt="logo" />
-            </a>
-
-            <button
-              className="navbar-toggler shadow-none border-0"
-              type="button"
-              data-bs-toggle="collapse"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-          </div>
-
-          <div className="collapse navbar-collapse justify-content-center flex-grow-1">
-            <div className="navbar-nav fsr-15">
-              <a className="nav-link active px-4 mx-1" href="/#">
-                <i className="fas fa-home"></i>
+    <>
+      <header>
+        <nav className="navbar navbar-expand-sm navbar-light bg-white shadow-sm py-0">
+          <div className="container-fluid">
+            <div className="ms-1 flex-grow-1 py-2">
+              <a className="navbar-brand" href="/#">
+                <img src={logo} alt="logo" />
               </a>
-              <a className="nav-link px-4 mx-1" href="/#">
-                <i className="fas fa-user-friends"></i>
-              </a>
-            </div>
-          </div>
 
-          <div className="d-flex justify-content-end align-items-center flex-grow-1 me-1">
-            <button className="profile-icon btn shadow-none rounded-pill p-1 d-flex align-items-center">
-              <img
-                className="rounded-circle"
-                src="https://images.pexels.com/photos/654690/pexels-photo-654690.jpeg"
-                width="28"
-                height="28"
-                alt="user"
-              />
-              <span className="ms-2 fw-bold me-1">Han</span>
-            </button>
-            <div className="menu-icon dropdown ms-2">
               <button
-                className="btn rounded-circle shadow-none"
-                data-bs-toggle="dropdown"
+                className="navbar-toggler shadow-none border-0"
+                type="button"
+                data-bs-toggle="collapse"
               >
-                <i className="fas fa-caret-down"></i>
+                <span className="navbar-toggler-icon"></span>
               </button>
-              <ul className="dropdown-menu dropdown-menu-end px-2 border-0 mt-1">
-                <li>
-                  <a
-                    className="dropdown-item p-2 d-flex align-items-center"
-                    href="/#"
-                  >
-                    <img
-                      className="rounded-circle me-3"
-                      src="https://images.pexels.com/photos/654690/pexels-photo-654690.jpeg"
-                      width="60"
-                      height="60"
-                      alt="user"
-                    />
-                    <div className="d-flex flex-column">
-                      <span className="fsr-125 fw-bold">Han So Yoon</span>
-                      <span className="text-muted">See your profile</span>
-                    </div>
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider mx-2 border-1" />
-                </li>
-                <li>
-                  <button
-                    className="dropdown-item d-flex align-items-center p-2"
-                    type="button"
-                  >
-                    <i className="fas fa-sign-out-alt me-3 fs-5 rounded-circle p-2" />
-                    <span>Log Out</span>
-                  </button>
-                </li>
-              </ul>
+            </div>
+
+            <div className="collapse navbar-collapse justify-content-center flex-grow-1">
+              <div className="navbar-nav fsr-15">
+                <a className="nav-link active px-4 mx-1" href="/#">
+                  <i className="fas fa-home"></i>
+                </a>
+                <a className="nav-link px-4 mx-1" href="/#">
+                  <i className="fas fa-user-friends"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className="d-flex justify-content-end align-items-center flex-grow-1 me-1">
+              <a
+                href="/#"
+                className="profile-icon rounded-pill p-1 d-flex align-items-center text-decoration-none"
+              >
+                <img
+                  className="rounded-circle"
+                  src="https://images.pexels.com/photos/654690/pexels-photo-654690.jpeg"
+                  width="28"
+                  height="28"
+                  alt="user"
+                />
+                <span className="ms-2 text-dark fw-bold me-1">Han</span>
+              </a>
+              <div className="menu-icon dropdown ms-2">
+                <button
+                  className="btn rounded-circle shadow-none"
+                  data-bs-toggle="dropdown"
+                >
+                  <i className="fas fa-caret-down"></i>
+                </button>
+                <ul className="dropdown-menu dropdown-menu-end px-2 border-0 mt-1">
+                  <li>
+                    <a
+                      className="dropdown-item p-2 d-flex align-items-center"
+                      href="/#"
+                    >
+                      <img
+                        className="rounded-circle me-3"
+                        src="https://images.pexels.com/photos/654690/pexels-photo-654690.jpeg"
+                        width="60"
+                        height="60"
+                        alt="user"
+                      />
+                      <div className="d-flex flex-column">
+                        <span className="fsr-125 fw-bold">Han So Yoon</span>
+                        <span className="text-muted">See your profile</span>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider mx-2 border-1" />
+                  </li>
+                  <li>
+                    <button
+                      className="dropdown-item d-flex align-items-center p-2"
+                      type="button"
+                    >
+                      <i className="fas fa-sign-out-alt me-3 fs-5 rounded-circle p-2" />
+                      <span>Log Out</span>
+                    </button>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
+    </>
   );
 }
 
